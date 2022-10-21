d327fa1abe91c54bff924f600b51a7a7b331328c
DANNNN <danielemerald@ymail.com>
2022-10-21 20:29:58 +0700

    Completed Contact layout

--- Homepage/home.js
+++ Homepage/home.js
@@ -3,11 +3,6 @@ window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
-    // if (currentScrollPos <= 150) {
-    //   document.getElementById("navbar").style.borderBottom = "none";
-    // } else {
-    //   document.getElementById("navbar").style.borderBottom = "1px solid white";
-    // }
   } else {
     document.getElementById("navbar").style.top = "-150px";
   }
