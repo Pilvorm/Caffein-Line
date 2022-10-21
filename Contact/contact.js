168178952c49135232c37766971f9d90827bcdbf
DANNNN <danielemerald@ymail.com>
2022-10-22 00:56:24 +0700

    Completed Contact Added Payment

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,3 +1,22 @@
+var mainScrollArea = document.getElementsByClassName('mdl-layout__content')[0];
+var scrollTimeout;
+
+window.onload = function () {
+  if (window.location.href == localStorage.getItem('lastUrl')) {
+    mainScrollArea.scrollTop = localStorage.getItem('scrollTop');
+  } else {
+    localStorage.setItem('lastUrl', window.location.href);
+    localStorage.setItem('scrollTop', 0);
+  }
+}
+
+mainScrollArea.addEventListener('scroll', function () {
+  clearTimeout(scrollTimeout);
+  scrollTimeout = setTimeout(function () {
+    localStorage.setItem('scrollTop', mainScrollArea.scrollTop);
+  }, 100)
+});
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
@@ -9,21 +28,30 @@ window.onscroll = function () {
   prevScrollpos = currentScrollPos;
 };
 
-var mainScrollArea = document.getElementsByClassName('mdl-layout__content')[0];
-var scrollTimeout;
+function checkField() {
+  var name = document.getElementById("name").value;
+  var invName = document.getElementById("invalid-name-warning");
 
-window.onload = function () {
-  if(window.location.href == localStorage.getItem('lastUrl')) {
-    mainScrollArea.scrollTop = localStorage.getItem('scrollTop');
-  } else {
-    localStorage.setItem('lastUrl', window.location.href);
-    localStorage.setItem('scrollTop', 0);
+  if(name == "") {
+    invName.innerHTML = 'Required';
   }
-}
 
-mainScrollArea.addEventListener('scroll', function() {
-  clearTimeout(scrollTimeout);
-  scrollTimeout = setTimeout(function() {
-    localStorage.setItem('scrollTop', mainScrollArea.scrollTop);
-  }, 100)
-});
+  var email = document.getElementById("email").value;
+  var invEmail = document.getElementById("invalid-email-warning");
+
+  if(email != "") {
+    if(!email.includes('@')) invEmail.innerHTML = 'Requires @'
+  }
+
+  if(email == "") {
+    invEmail.innerHTML = 'Required';
+  }
+
+  var msg = document.getElementById("message").value;
+  var invMessage = document.getElementById("invalid-message-warning");
+
+  if(msg == "") {
+    invMessage.innerHTML = 'Required';
+  }
+
+}
